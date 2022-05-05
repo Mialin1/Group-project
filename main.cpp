aeeffa964a113dd10855d8376ab3a523430124be
Mialin <linmia2002@gmail.com>
2022-05-06 01:30:54 +0800

    main

--- main.cpp
+++ main.cpp
@@ -9,13 +9,8 @@
 #include <thread>
 
 using namespace std;
-<<<<<<< HEAD
+
 extern vector<vector<Map> > maps;
-=======
-
-extern vector<vector<Map>> maps;
-
->>>>>>> c0042af0ba7b83fed448841e5d54180ef8c5f157
 int main(){
 
     Player player;
