52d4d918ca4c4a37f98abc9bc1543d884419a0cc
yyanghly <leyanghly@gmail.com>
2022-05-07 10:54:18 +0800

    update

--- map.cpp
+++ map.cpp
@@ -10,13 +10,14 @@ void Map::print_map(Point position){
     player_image[2]=" ^ ";
     for(int i = 0; i < len_x; i++)
     for(int _i = 0; _i < RANGE_X; _i++){         //i-th row of the map
-        cout << "\u25BC ";
+        string s = "\u25BC ";
         for(int j = 0; j < len_y; j++) //j-th column of the map
             if(position.x==i && position.y==j)
-                cout << player_image[_i];
+                s += player_image[_i];
             else 
-                cout << map[i][j].image.s[_i]; 
-        cout << " \u25BC" << endl;
+                s += map[i][j].image.s[_i]; 
+        s += " \u25BC";
+        cout << s << endl;
     }
     
 }
